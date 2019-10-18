65e37d1f527b0dbf473135e11a1a293a7dbba4ef
anweshb <54733708+anweshb@users.noreply.github.com>
2019-10-18 19:32:04 +0530

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hj*1hto^e#w5hz!5(v9ahmzm63uo)c2wx^+!_*0ef8*jlgywoi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anweshb.pythonanywhere.com']
 
 
 # Application definition
